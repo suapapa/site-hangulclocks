ef026158846cdd59b185653d85390f73c11ad1d0
Homin Lee <homin.lee@suapapa.net>
2018-12-11 22:04:40 +0900

    fix... (don't know what to do..)

--- src/components/HangulClocks.vue
+++ src/components/HangulClocks.vue
@@ -2,16 +2,6 @@
   <v-container>
     <v-layout text-xs-center wrap >
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
 
       <v-flex xs12 mb-5 >
         <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
@@ -27,6 +17,26 @@
             {{ eco.text }}
           </a>
         </v-layout>
+      </v-flex>
+
+      <v-flex xs12 mb-5 v-for="(clk, i) in hangulclocks" :key="i">
+        <v-card> 
+          <v-img
+            :src="require('../assets/hangulclock_hominlee.jpg')"
+            aspect-ratio="2.75"
+          ></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ clk.title }}</h3>
+              <div>{{ clk.author }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn flat color="orange">Site</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
 
     </v-layout>
@@ -54,13 +64,13 @@
         {
           title: '너가 있어 아름다운 오늘, 한글시계',
           author: "이상훈",
-          image: 'assets/hangulclock_sanghunlee.png',
+          image: '../assets/hangulclock_sanghunlee.png',
           href: 'https://hangulclock.today/'
         },
         {
           title: '한글시계(HangulClock)',
           author: "이호민",
-          image: 'hangulclock_hominlee.jpg',
+          image: '../assets/hangulclock_hominlee.jpg',
           href: 'https://github.com/suapapa/hangulclock'
         },
     ]
