9e63839247b9a204a30cbe7727b0a6ea899d266a
Homin Lee <homin.lee@suapapa.net>
2018-12-12 11:21:01 +0900

    fix to show corrent image

--- src/components/HangulClocks.vue
+++ src/components/HangulClocks.vue
@@ -2,27 +2,10 @@
   <v-container>
     <v-layout text-xs-center wrap >
 
-
-      <v-flex xs12 mb-5 >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
       <v-flex xs12 mb-5 v-for="(clk, i) in hangulclocks" :key="i">
-        <v-card> 
+        <v-card>
           <v-img
-            :src="require('../assets/hangulclock_hominlee.jpg')"
+            :src="`${clk.image}`"
             aspect-ratio="2.75"
           ></v-img>
 
@@ -64,13 +47,13 @@
         {
           title: '너가 있어 아름다운 오늘, 한글시계',
           author: "이상훈",
-          image: '../assets/hangulclock_sanghunlee.png',
+          image: require('../assets/hangulclock_sanghunlee.png'),
           href: 'https://hangulclock.today/'
         },
         {
           title: '한글시계(HangulClock)',
           author: "이호민",
-          image: '../assets/hangulclock_hominlee.jpg',
+          image: require('../assets/hangulclock_hominlee.jpg'),
           href: 'https://github.com/suapapa/hangulclock'
         },
     ]
